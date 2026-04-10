39f69660ba514726ad5ba15ece1ebf554cf7a55c
Maximo <maximoaredes.iacc@gmail.com>
2026-04-10 15:16:31 -0300

    Entrega final proyecto Django: blog de viajes con usuarios, perfiles, mensajeria y CKEditor

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
+from django.db.models import Q
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 
 
@@ -9,6 +10,38 @@ class PostListView(ListView):
     template_name = "blog/post_list.html"
     context_object_name = "posts"
 
+    def get_queryset(self):
+        busqueda = self.request.GET.get("busqueda", "")
+
+        if self.request.user.is_staff:
+            queryset = Post.objects.exclude(estado="ARCHIVADO")
+        else:
+            queryset = Post.objects.filter(estado="PUBLICADO")
+
+        if busqueda:
+            queryset = queryset.filter(
+                Q(titulo__icontains=busqueda) |
+                Q(subtitulo__icontains=busqueda) |
+                Q(contenido__icontains=busqueda)
+            )
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["search_query"] = self.request.GET.get("busqueda", "")
+
+        cantidad = len(context["posts"])
+        resto = cantidad % 3
+
+        if cantidad == 0:
+            context["placeholders"] = 0
+        elif resto == 0:
+            context["placeholders"] = 0
+        else:
+            context["placeholders"] = 3 - resto
+
+        return context
 
 class PostDetailView(DetailView):
     model = Post
@@ -16,7 +49,7 @@ class PostDetailView(DetailView):
     context_object_name = "post"
 
 
-class PostCreateView(LoginRequiredMixin, CreateView):
+class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
     template_name = "blog/post_form.html"
     fields = ["titulo", "subtitulo", "contenido", "imagen", "estado"]
@@ -26,15 +59,24 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.autor = self.request.user
         return super().form_valid(form)
 
+    def test_func(self):
+        return self.request.user.is_staff
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = "blog/post_form.html"
     fields = ["titulo", "subtitulo", "contenido", "imagen", "estado"]
     success_url = reverse_lazy("blog:post_list")
 
+    def test_func(self):
+        return self.request.user.is_staff
 
-class PostDeleteView(LoginRequiredMixin, DeleteView):
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = "blog/post_confirm_delete.html"
     success_url = reverse_lazy("blog:post_list")
+
+    def test_func(self):
+        return self.request.user.is_staff
--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
-
 app_name = "main"
 
 urlpatterns = [
@@ -11,12 +10,6 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("perfil/", views.perfil, name="perfil"),
     path("editar-perfil/", views.editar_perfil, name="editar_perfil"),
-
-    path("login/", auth_views.LoginView.as_view(
-        template_name="registration/login.html"
-    ), name="login"),
-
-    path("logout/", auth_views.LogoutView.as_view(
-        next_page="main:index"
-    ), name="logout"),
+    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="main:index"), name="logout"),
 ]
